446837333fd18cbfd38fd8574403e68dc8400d69
cathaysia <DragonBillow@outlook.com>
2022-03-02 00:21:09 +0800

    feat(root.go): add ICMPPing

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
+	"net"
 	"net/http"
 	"sync"
 	"time"
@@ -31,21 +34,20 @@ var rootCmd = &cobra.Command{
 	Long:  `a ping tool for http/https`,
 	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args[0]) < 4 {
-			return &ParameterError{
-				message: fmt.Sprintf("url 格式不正确：%v", args[0]),
-			}
-		}
-		if args[0][0:4] != "http" {
-			return &ParameterError{
-				message: fmt.Sprintf("url 不以 http 开头：%v", args[0]),
-			}
-		}
-		for i := 0; i < count; i++ {
-			waitGroup.Add(1)
-			time.Sleep(time.Second * time.Duration(interval))
+		if len(args[0]) > 4 && args[0][0:4] == "http" {
+			for i := 0; i < count; i++ {
+				waitGroup.Add(1)
+				time.Sleep(time.Second * time.Duration(interval))
 
-			go httpPing(args[0])
+				go httpPing(args[0])
+			}
+		} else {
+			for i := 0; i < count; i++ {
+				waitGroup.Add(1)
+				time.Sleep(time.Second * time.Duration(interval))
+
+				go ICMPPing(args[0])
+			}
 		}
 		waitGroup.Wait()
 
@@ -83,6 +85,97 @@ func httpPing(url string) {
 	}
 
 	response.Body.Close()
+
+	fmt.Printf("ping %v: %v ms\n", url, duration)
+}
+
+// https://www.cnblogs.com/wlw-x/p/14169607.html
+type ICMP struct {
+	Type        uint8
+	Code        uint8
+	CheckSum    uint16
+	Identifier  uint16
+	SequenceNum uint16
+}
+
+func CheckSum(data []byte) (rt uint16) {
+	var (
+		sum    uint32
+		index  int
+		length = len(data)
+	)
+
+	for length > 1 {
+		sum += uint32(data[index])<<8 + uint32(data[index+1])
+		index += 2
+		length -= 2
+	}
+
+	if length > 0 {
+		sum += uint32(data[index]) << 8
+	}
+
+	rt = uint16(sum) + uint16(sum>>16)
+
+	return ^rt
+}
+
+func ICMPPing(url string) {
+    defer waitGroup.Done()
+	raddr, err := net.ResolveIPAddr("ip", url)
+	if err != nil {
+		panic(err)
+	}
+
+	laddr := net.IPAddr{
+		IP: net.ParseIP("0.0.0.0"),
+	}
+
+	conn, err := net.DialIP("ip4:icmp", &laddr, raddr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	var (
+		buffer      bytes.Buffer
+		icmp        = ICMP{8, 0, 0, 0, 0}
+		originBytes = make([]byte, 2000)
+	)
+
+	err = binary.Write(&buffer, binary.BigEndian, icmp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = binary.Write(&buffer, binary.BigEndian, originBytes[0:64])
+	if err != nil {
+		panic(err)
+	}
+
+	b := buffer.Bytes()
+	binary.BigEndian.PutUint16(b[2:], CheckSum(b))
+
+	_, err = conn.Write(buffer.Bytes())
+
+	if err != nil {
+		panic(err)
+	}
+
+	start := time.Now().UnixNano()
+	recv := make([]byte, 1024)
+
+	if err = conn.SetReadDeadline(time.Now().Add(time.Second * 3)); err != nil {
+		panic(err)
+	}
+
+	if _, err = conn.Read(recv); err != nil {
+		panic(err)
+	}
+
+	duration := (time.Now().UnixNano() - start) / 1e6
 
 	fmt.Printf("ping %v: %v ms\n", url, duration)
 }
