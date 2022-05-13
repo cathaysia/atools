a24efeb22ad71ce7002ac4537d4508a6421ac1e5
cathaysia <DragonBillow@outlook.com>
2022-05-13 21:47:53 +0800

    fix(ipaddress): 修复了 ipaddress 的错误返回问题

--- internal/ipaddress.go
+++ internal/ipaddress.go
@@ -3,36 +3,51 @@ package internal
 import (
 	"context"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func ErrHTTPS(err error) error {
-	return fmt.Errorf("ErrHTTPS: %w", err)
+type IPAddressError struct {
+	msg string
+}
+
+func NewIPAddressErr(err error, msg string) error {
+	if err != nil {
+		msg = fmt.Sprintf("%v", err.Error()) + msg
+	}
+
+	return &IPAddressError{
+		msg: fmt.Sprintf("IPAddressError: %v", msg),
+	}
+}
+
+func (ipaddressErr *IPAddressError) Error() string {
+	return ipaddressErr.msg
 }
 
 func IPAddress(url string) (map[string]string, error) {
 	request, err := http.NewRequestWithContext(context.Background(), "GET", "https://www.ipaddress.com/search/"+url, nil)
 	if err != nil {
-		return nil, ErrHTTPS(err)
+		return nil, NewIPAddressErr(err, "")
 	}
 
 	request.Header.Add("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66`)
 
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return nil, ErrHTTPS(err)
+		return nil, NewIPAddressErr(err, "")
 	}
 
 	defer response.Body.Close()
 
 	if response.StatusCode != 200 {
-		return nil, ErrHTTPS(err)
+		return nil, NewIPAddressErr(nil, fmt.Sprintf("HTTP 状态码为 %v 而不是 200", response.StatusCode))
 	}
 
 	doc, err := goquery.NewDocumentFromReader(response.Body)
 	if err != nil {
-		return nil, ErrHTTPS(err)
+		return nil, NewIPAddressErr(err, "")
 	}
 
 	result := make(map[string]string)
